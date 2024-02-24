33439cafd4aa41ef73b8a351d4602aff110cedc1
paul0sham <apawlos0@gmail.com>
2024-02-24 22:58:19 +0300

    updated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main(): prints a statemet using prints
+ * returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}

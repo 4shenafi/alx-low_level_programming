a939c9e4073a8fdd381a9d92051853887cb87a9f
paul0sham <apawlos0@gmail.com>
2024-02-24 03:10:26 -0800

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
-/**
- * this program prints a statement using puts function
- *
- * and finally returns 0 always
+/*
+ *the main functions prints using puts function
+ *and returns 0
  */
-
+/*
+ *main function prints a statement
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

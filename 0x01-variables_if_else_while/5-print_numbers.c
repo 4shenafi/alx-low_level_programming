6a27b80c0ad3db68a1f3f8d96b9cbc1ae8129039
paul0sham <apawlos0@gmail.com>
2024-02-25 12:58:48 +0300

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - main function prints 0-9 digit of base 10
+  * Return: returns 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d ", n);
+	}
+	printf("\n");
+	return (0);
+}

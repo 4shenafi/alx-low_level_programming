ac2b1b9d30a96bdf3bda02307380db97b3de4966
paul0sham <apawlos0@gmail.com>
2024-02-26 23:51:21 +0300

    updated

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * _abs - changes the value into positive
+  * Return: always return value in abs value
+  */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (0);
+	else
+		return (-r);
+}

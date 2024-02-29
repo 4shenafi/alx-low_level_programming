b7736d6e11e7abd34ddeebec26ee285decff8445
paul0sham <apawlos0@gmail.com>
2024-02-29 11:39:16 +0300

    updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,26 +5,28 @@
  */
 void times_table(void)
 {
-	int r;
-	int c;
-	int p;
+    int row, col, product;
 
-	for (r = 0; r < 10; r++)
-	{
-		for (c = 0; c < 10; c++)
-		{
-			p = r * c;
-			if (c > 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (p < 10)
-				_putchar(' ');
-			else
-				_putchar((p / 10) + '0');
-			_putchar((p % 10) + '0');
-		}
-		_putchar('\n');
-	}
+    for (row = 0; row < 10; row++)
+    {
+        for (col = 0; col < 10; col++)
+        {
+            product = row * col;
+
+            if (col > 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+
+            if (product < 10)
+                _putchar(' ');
+            else
+                _putchar((product / 10) + '0');
+
+            _putchar((product % 10) + '0');
+        }
+        _putchar('\n');
+    }
 }
+

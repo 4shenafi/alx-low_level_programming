cb282d879296cc96f6b0f686f327263cdfcc4508
paul0sham <apawlos0@gmail.com>
2024-03-02 23:10:06 +0300

    updated

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - prints Fibonacci sequence
+  * Return: always return 0
+  */
+int main(void)
+{
+	unsigned long int i = 1;
+	unsigned long int j = 2;
+	unsigned long int n;
+	int k;
+
+	printf("%lu, %lu, ", i, j);
+
+	for (k = 3; k <= 98; k++)
+	{
+		n = i + j;
+		printf("%lu", n);
+		if (k < 98)
+			printf(", ");
+		i = j;
+		j = n;
+	}
+	printf("\n");
+	return (0);
+}

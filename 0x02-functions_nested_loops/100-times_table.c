cc3e3f5c633be3ce802615d27046454ab2e5ef66
paul0sham <apawlos0@gmail.com>
2024-03-02 21:47:59 +0300

    updted

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,38 +1,40 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  *
+  * print_times_table - a function prints time table
+  * @n: passed max table num
   */
 void print_times_table(int n)
 {
-    int row, col, prod;
+	int row;
+	int col;
+	int prod;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (row = 0; row <= n; row++)
-        {
-            for (col = 0; col <= n; col++)
-            {
-                prod = col * row;
-                if (col == 0)
-                {
-                    printf("%d", prod);
-                } 
-                else if (prod < 10 && col != 0)
-                {
-                    printf(",   %d", prod);
-                } 
-                else if (prod >= 10 && prod < 100)
-                {
-                    printf(",  %d", prod);
-                } 
-                else if (prod >= 100)
-                {
-                    printf(", %d", prod);
-                }
-            }
-            printf("\n");
-        }
-    }
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				prod = col * row;
+				if (col == 0)
+				{
+					printf("%d", prod)
+				}
+				else if (prod < 10 && col != 0)
+				{
+					printf(",   %d", prod);
+				}
+				else if (prod >= 10 && prod < 100)
+				{
+					printf(",  %d", prod);
+				}
+				else if (prod >= 100)
+				{
+					printf(", %d", prod);
+				}
+			}
+			printf("\n");
+		}
+	}
 }
-

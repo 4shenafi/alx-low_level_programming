b8c338a4947d96b3727e687cd8c8364f70a6a053
paul0sham <apawlos0@gmail.com>
2024-03-02 21:23:07 +0300

    updated

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *
+  */
+void print_times_table(int n)
+{
+	int col;
+	int row;
+	int prod;
+
+	if (n <= 15)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			for (row = 0; row <= n; row++)
+			{
+				prod = col * row;
+				if (row < n)
+					printf("%4d,", prod);
+				else
+					printf("%4d", prod);
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+}
+

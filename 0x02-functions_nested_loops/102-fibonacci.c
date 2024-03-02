e2259dc90170f2ce517eece4d790255d4d98c8a1
paul0sham <apawlos0@gmail.com>
2024-03-02 22:36:40 +0300

    updated

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,22 +6,23 @@
   */
 int main(void)
 {
-	int n;
-	int k;
-	int i = 1;
-	int j = 2;
+	unsigned long int i = 1;
+	unsigned long int j = 2;
+	unsigned long int n;
 
-	printf("%d, %d, ", i, j);
-	for (k = 2; k < 50; k++)
+	printf("%lu, %lu, ", i, j);
+
+	for (int k = 3; k <= 50; k++)
 	{
 		n = i + j;
-		printf("%d", n);
+		printf("%lu", n);
 		if (k < 50)
 			printf(", ");
 		i = j;
 		j = n;
 	}
 	printf("\n");
-	return 0;
+	return (0);
 }
+
 

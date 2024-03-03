d05fb272cac16f6199616eef2bf5927d13886787
paul0sham <apawlos0@gmail.com>
2024-03-03 21:58:45 +0300

    multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * mul - calculates multiplication
+  * @a: first argument
+  * @b: second argument
+  * Return: returns product
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}

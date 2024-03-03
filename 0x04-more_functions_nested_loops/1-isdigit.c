2c43796938acd81c7c5de91cc802512e5c31e800
paul0sham <apawlos0@gmail.com>
2024-03-03 21:51:52 +0300

    digit checker

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * is_digit - checkes in between
+  * @c: passed parameter
+  * Return: returns either 1 or 0
+  */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

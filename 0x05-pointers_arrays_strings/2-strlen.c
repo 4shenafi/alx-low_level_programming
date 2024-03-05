793e4de331438aa1e134b8d945824681b3952c11
paul0sham <apawlos0@gmail.com>
2024-03-05 13:49:48 +0300

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strlen - gives the length of string
+  * @s: passed parameter
+  * Return: returns the length of dtring
+  */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

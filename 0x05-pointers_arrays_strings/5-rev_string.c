41589309f7da9ae670b68ff91945c98396a03263
paul0sham <apawlos0@gmail.com>
2024-03-05 14:19:12 +0300

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,12 +8,14 @@
   */
 void rev_string(char *s)
 {
-	int l = strlen(s);
-	int i;
+	int length = strlen(s);
+	int i, j;
+	char temp;
 
-	for (i = l - 1; i >= 0; --i)
+	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
-		putchar(s[i]);
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 	}
-	printf("\n");
 }

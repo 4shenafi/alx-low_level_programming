b5350f41cb54d168f95082217e077a3e1faf4b35
paul0sham <apawlos0@gmail.com>
2024-03-06 10:25:08 +0300

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+  * _strlen_recursion - returns recursion
+  * @s: passed parameter
+  * Return: returns
+  */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}

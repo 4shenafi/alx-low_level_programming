57e60364f1ded7724eb0837cc43f639147a7752d
paul0sham <apawlos0@gmail.com>
2024-03-06 10:17:04 +0300

    recursion - 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _print_rev_recursion - returns recursion
+  * @s: passed parameter
+  * Return: returns
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s+1);
+
+	putchar(*s);
+}

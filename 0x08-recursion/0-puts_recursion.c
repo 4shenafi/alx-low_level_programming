8284c59e5409a958e083c2abc38401c2e57e9057
paul0sham <apawlos0@gmail.com>
2024-03-06 09:56:37 +0300

    task-0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _puts_recursion - returns recursion
+  * @s: passed parameter
+  * Return: returns
+  */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void _puts_recursion(char*);
+void _print_rev_recursion(char*);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int, int);
+int is_prime_number(int);
+
+#endif

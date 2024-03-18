1dee2f1aa58b328fff26ed2ae5085249372f193a
paul0sham <apawlos0@gmail.com>
2024-03-18 13:59:01 +0300

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - a function prints name of file
- * Return: always return
- */
-int main(int argc, char* argv[])
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: Always return zero
+  */
+int main(int argc, char *argv[])
 {
-    (void) argc;
-    printf("%s", *argv);
-    return (0);
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }

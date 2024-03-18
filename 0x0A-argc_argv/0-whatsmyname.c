e7fe6195d5e0add0d6925227471ffd68b3aed912
paul0sham <apawlos0@gmail.com>
2024-03-18 13:50:06 +0300

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,9 @@
  * main - a function prints name of file
  * Return: always return
  */
-int mian(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
     (void) argc;
-    printf("%s", argv[0]);
+    printf("%s", *argv);
     return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int mian(int argc, char* argv[]);
+int _putchar(char c);
+int _atoi(char *s);
 
 #endif

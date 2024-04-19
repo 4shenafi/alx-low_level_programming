8d533564cd129e8e1787138ec6e7bf14844046fe
paul0sham <apawlos0@gmail.com>
2024-04-19 18:05:35 +0300

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,8 +20,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("%s\n", h->str);
-			printf("%u\n", h->len);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		count++;
